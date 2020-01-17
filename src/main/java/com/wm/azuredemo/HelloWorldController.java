f811590333ec6ab625336785bca01ad49861c5a9
sarthaksarathi <sarthaksarathi.12@gmail.com>
2020-01-17 21:02:57 +0000

    Base checkin

---
+++ src/main/java/com/wm/azuredemo/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.wm.azuredemo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@Autowired
+	DBConnection dbconnHelper;
+	
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path = "get-inventory")
+	public List<Inventory> getInventory(){
+		return dbconnHelper.getInventory();
+	}
+}
